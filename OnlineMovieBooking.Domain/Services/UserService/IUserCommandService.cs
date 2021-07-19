d3fab53633f9a9148389784cb6c1f206aa0977e7
Shashank Gajula <shashanknetha12345@gmail.com>
2021-07-19 19:36:20 +0530

    Added UserService

---
+++ OnlineMovieBooking.Domain/Services/UserService/IUserCommandService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OnlineMovieBooking.Domain;
+using OnlineMovieBooking.Domain.DTO;
+
+namespace OnlineMovieBooking.Domain.Services.UserService
+{
+    public interface IUserCommandService
+    {
+       
+        void AddUser(User user);
+        void Delete(int id);
+        void Edit(int id, User u);
+    }
+}
---
+++ OnlineMovieBooking.Domain/Services/UserService/UserQueryService.cs
@@ -0,0 +1,35 @@
+﻿using OnlineMovieBooking.Domain.DTO;
+using OnlineMovieBooking.Domain.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineMovieBooking.Domain.Services.UserService
+{
+    public class UserQueryService : IUserQueryService
+    {
+        private readonly IUserRepository repository;
+        private UserRepository ur = new UserRepository(); 
+
+
+        public UserQueryService(IUserRepository repository)
+        {
+            this.repository = repository;
+        }
+
+
+        public User Find(int id)
+        {
+           Repository.Entities.User user = ur.GetById(id);
+            User u = new User();
+            u.UserId = user.UserId;
+            u.Username = user.Username;
+            u.Email = user.Email;
+            u.MobileNo = user.MobileNo;
+            u.Password = user.Password;
+            return u;
+        }
+    }
+}
