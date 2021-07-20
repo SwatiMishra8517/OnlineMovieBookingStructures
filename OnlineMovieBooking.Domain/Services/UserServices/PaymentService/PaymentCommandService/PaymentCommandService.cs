dce9f5ceeed1b4fe96bb8e4e2318e6dce57811d3
Shashank Gajula <shashanknetha12345@gmail.com>
2021-07-20 20:29:12 +0530

    ignore the last one now services part is completed

--- OnlineMovieBooking.Domain/Services/UserServices/PaymentService/PaymentCommandService/PaymentCommandService.cs
+++ OnlineMovieBooking.Domain/Services/UserServices/PaymentService/PaymentCommandService/PaymentCommandService.cs
@@ -8,7 +8,20 @@ using OnlineMovieBooking.Domain.Repository;
 
 namespace OnlineMovieBooking.Domain.Services.UserServices.PaymentService.PaymentCommandService
 {
-    class PaymentCommandService:IPaymentCommandService
+    class PaymentCommandService : IPaymentCommandService
     {
+        private PaymentRepository pr = new PaymentRepository();
+        public void Add(Payment rp)
+        {
+            Repository.Entities.Payment dp = new Repository.Entities.Payment();
+            dp.PaymentId = rp.PaymentId;
+            dp.Amount = rp.Amount;
+            dp.Time = rp.Time;
+            dp.DiscountCouponId = rp.DiscountCouponId;
+            dp.RemoteTransactionId = rp.RemoteTransactionId;
+            dp.PaymentMethod = rp.PaymentMethod;
+            dp.BookingId = rp.BookingId;
+            pr.Add(dp);
+        }
     }
 }
