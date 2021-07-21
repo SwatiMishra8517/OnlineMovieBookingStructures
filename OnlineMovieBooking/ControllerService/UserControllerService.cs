96e8e3b4ece3ee56bf4aa4edabf54a8f68e7ccdf
Shashank Gajula <shashanknetha12345@gmail.com>
2021-07-21 12:07:51 +0530

    done with usercontrollerservice for your refernce

--- OnlineMovieBooking/ControllerService/UserControllerService.cs
+++ OnlineMovieBooking/ControllerService/UserControllerService.cs
@@ -3,10 +3,41 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using OnlineMovieBooking.Proxy;
+using OnlineMovieBooking.Models;
 
 namespace OnlineMovieBooking.ControllerService
 {
     public class UserControllerService
     {
+        private UserProxy userProxy = new UserProxy();
+        public void Add(UserModel user)
+        {
+            userProxy.Add(user);
+        }
+        public void Delete(int id)
+        {
+            userProxy.Delete(id);
+        }
+        public void Update(int id, UserModel user)
+        {
+            userProxy.Update(id, user);
+        }
+        public UserModel GetById(int id)
+        {
+            return userProxy.GetById(id);
+        }
+        public List<UserModel> GetAll()
+        {
+            return userProxy.GetAll();
+        }
+        public UserModel GetByUserName(string username)
+        {
+            return userProxy.GetByUserName(username);
+        }
+        public UserModel GetByEmail(string email)
+        {
+            return userProxy.GetByEmail(email);
+        }
     }
 }
