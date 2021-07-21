e5f8c53c0e63674800fe4f5647f5b5545e0c4594
Shashank Gajula <shashanknetha12345@gmail.com>
2021-07-21 13:10:34 +0530

    trying to solve conflict

--- OnlineMovieBooking/ControllerService/PaymentControllerService.cs
+++ OnlineMovieBooking/ControllerService/PaymentControllerService.cs
@@ -1,5 +1,4 @@
 ﻿using OnlineMovieBooking.Proxy;
-using OnlineMovieBooking.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,47 +8,6 @@ namespace OnlineMovieBooking.ControllerService
 {
     public class PaymentControllerService
     {
-        private PaymentProxy paymentProxy = new PaymentProxy();
-        public void Add(PaymentModel payment)
-        {
-            paymentProxy.Add(payment);
-        }
-        public void Delete(int id)
-        {
-            paymentProxy.Delete(id);
-        }
-        public void Update(int id, PaymentModel payment)
-        {
-            paymentProxy.Update(id, payment);
-        }
-        public PaymentModel GetById(int id)
-        {
-            return paymentProxy.GetById(id);
-        }
-        public List<PaymentModel> GetAll()
-        {
-            return paymentProxy.GetAll();
-        }
-        public List<PaymentModel> GetByUserId(int id)
-        {
-            return paymentProxy.GetByUserId(id);
-        }
-        public List<PaymentModel> GetByShowId(int id)
-        {
-            return paymentProxy.GetByShowId(id);
-        }
-
-        public PaymentModel GetByTransactionId(string id)
-        {
-            return paymentProxy.GetByTransactionId(id);
-        }
-        public List<PaymentModel> GetByPaymentMethod(string method)
-        {
-            return paymentProxy.GetByPaymentMethod(method);
-        }
-        public List<PaymentModel> GetByBookingId(int id)
-        {
-            return paymentProxy.GetByBookingId(id);
-        }
+        
     }
 }
